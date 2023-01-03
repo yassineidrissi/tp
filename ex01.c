51aeeb1f3c8795415d1a66e0e7764e93cdd19ed5
yassineidrissi <yassine42idrissi@gmail.com>
2023-01-03 18:24:52 +0100

    finished

---
+++ ex01.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaidriss <yaidriss@student1337.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 17:50:06 by yaidriss          #+#    #+#             */
+/*   Updated: 2023/01/03 17:50:06 by yaidriss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	int A[10];
+	int s;
+	int i;
+	int n ;
+	s = 0;
+	printf("entrer les elts du tableau :");
+	scanf("%d",&n);
+	printf("entrer les valeurs du tableau :\n");
+	for(i = 0; i < n; i++)
+		scanf("%d", &A[i]);
+	printf("les valeurs de tableau sont:\n");
+	for(i= 0; i < n ; i++)
+		printf("%d\n",A[i]);
+	for(i = 0; i < n; i++)
+		s += A[i];
+	printf("la somme de toutes les valeurs est: %d", s);
+	return 0;
+	
+}
